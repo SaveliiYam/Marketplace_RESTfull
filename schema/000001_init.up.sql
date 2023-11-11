774330c3a6a8aa6d223462e123f1a0c626218bd2
SaveliiYam <savelkayamil5@gmail.com>
2023-11-11 12:49:46 +0300

    create indexes

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users
+CREATE TABLE IF NOT EXISTS users
 (
     id serial PRIMARY KEY not null,
     name varchar(255) not null,
@@ -7,18 +7,18 @@ CREATE TABLE users
     status boolean default false
 );
 
-CREATE TABLE categories(
+CREATE TABLE IF NOT EXISTS categories(
     id SERIAL PRIMARY KEY,
     title VARCHAR(50) not null
 );
 
-CREATE TABLE brands (
+CREATE TABLE IF NOT EXISTS brands (
     id SERIAL PRIMARY KEY,
     title VARCHAR(50) not null,
     description TEXT
 );
 
-CREATE TABLE products(
+CREATE TABLE IF NOT EXISTS products(
     id serial PRIMARY KEY,
     title VARCHAR(50) not null,
     description TEXT,
@@ -27,22 +27,18 @@ CREATE TABLE products(
     categories_id INTEGER REFERENCES categories (id) on delete cascade not null
 );
 
-CREATE TABLE baskets(
+CREATE TABLE IF NOT EXISTS baskets(
     id serial PRIMARY KEY,
     user_id INTEGER REFERENCES users (id) on delete cascade not null,
     product_id INTEGER REFERENCES products (id) on delete cascade not null
 );
 
+CREATE INDEX user_all ON users(id, name, username, password_hash, status);
+CREATE INDEX user_id_status ON users(id, status);
+CREATE INDEX user_status ON users(status);
 CREATE INDEX category_id_title ON categories (id, title);
 CREATE INDEX category_id ON categories(id);
 CREATE INDEX brands_id_title_description ON brands (id, title, description);
 CREATE INDEX brands_id ON brands(id);
 
--- INSERT INTO brands (title) values ('Nike');
--- INSERT INTO brands (title) values ('Adidas');
--- INSERT INTO brands (title) values ('Gucci');
-
--- INSERT INTO categories (title) values ('Спортивная обувь');
--- INSERT INTO categories (title) values ('Летняя обувь');
--- INSERT INTO categories (title) values ('Зимняя обувь');
 
